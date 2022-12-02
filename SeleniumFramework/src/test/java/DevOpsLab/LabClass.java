44e9b2d842e96b32bc7a9425beefb5703f0c5137
Priya Kaviyil <L00171183@atu.ie>
2022-12-02 12:00:00 +0000

    added java file

---
+++ SeleniumFramework/src/test/java/DevOpsLab/LabClass.java
@@ -0,0 +1,45 @@
+package DevOpsLab;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LabClass {
+	
+	WebDriver driver = null; 
+	
+	@BeforeTest
+	public void SetUp() {
+		
+		
+		System.setProperty("webdriver.chrome.driver","D:\\Eclipse\\SeleniumFramework\\Drivers\\chrome\\chromedriver.exe");
+		
+		//WebDriver driver = new FirefoxDriver(); 
+		
+		 driver = new ChromeDriver(); 
+		
+	}
+	
+	@Test
+    public void Search() {
+		
+		
+		driver.get("https://www.lyit.ie/Student-Hub");
+	}
+	
+	@AfterTest
+  
+    public void Close() {
+		
+		
+		driver.close();
+		
+		driver.quit();
+		
+		System.out.print("Test Completed Successfully");
+	}
+
+}
